fa61904c9d4261f38887e9f8f6cce6d8217809e1
Valentine <valentine.nazarov@gmail.com>
2022-06-01 15:20:11 +0300

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,26 @@
+import fs from "fs/promises";
+import path from "path";
+
 export const create = async () => {
-    // Write your code here 
+  const __dirname = path.resolve();
+  const file = path.resolve(__dirname, "files", "fresh.txt");
+  if (await isExists(file)) {
+    throw new Error("FS operation failed");
+  } else {
+    await fs.writeFile(
+      path.resolve(__dirname, "files", "fresh.txt"),
+      "I am fresh and young"
+    );
+  }
 };
+
+export const isExists = async (path) => {
+  try {
+    await fs.access(path);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+create().then();
