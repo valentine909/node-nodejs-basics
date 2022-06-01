fcb0d817797b74032130ac38ff8855e4af18d2d4
Valentine <valentine.nazarov@gmail.com>
2022-06-01 16:34:12 +0300

    feat: add delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,20 @@
-export const remove = async () => {
-    // Write your code here 
+import fs from "fs/promises";
+import { fsError, isExists } from "./helper.js";
+import path from "path";
+
+const settings = {
+  folder: "files",
+  filename: "fileToRemove.txt",
 };
+
+export const remove = async () => {
+  const __dirname = path.resolve();
+  const file = path.resolve(__dirname, settings.folder, settings.filename);
+  if (!(await isExists(file))) {
+    throw fsError;
+  } else {
+    await fs.rm(file);
+  }
+};
+
+remove().then();
