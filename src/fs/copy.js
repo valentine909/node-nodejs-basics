947d0d420c9c000829f52b9d0207f14e1540c82e
Valentine <valentine.nazarov@gmail.com>
2022-06-01 16:24:32 +0300

    feat: add copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,35 @@
-export const copy = async () => {
-    // Write your code here 
+import fs from "fs/promises";
+import path from "path";
+import { fsError, isExists } from "./helper.js";
+
+const settings = {
+  source: "files",
+  destination: "files_copy",
 };
+
+export const copy = async () => {
+  const __dirname = path.resolve();
+  const src = path.resolve(__dirname, settings.source);
+  const dest = path.resolve(__dirname, settings.destination);
+  if (await isExists(dest)) {
+    throw fsError;
+  } else {
+    await recursiveFilesCopy(src, dest);
+  }
+};
+
+const recursiveFilesCopy = async (src, dest) => {
+  await fs.mkdir(dest, { recursive: true });
+  const files = await fs.readdir(src, { withFileTypes: true });
+  for (const file of files) {
+    const newSrc = path.resolve(src, file.name);
+    const newDest = path.resolve(dest, file.name);
+    if (file.isDirectory()) {
+      await recursiveFilesCopy(newSrc, newDest);
+    } else {
+      await fs.copyFile(newSrc, newDest);
+    }
+  }
+};
+
+copy().then();
