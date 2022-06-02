09e691a455c445cdf4ba11abd933ad13d4e93dfe
Valentine <valentine.nazarov@gmail.com>
2022-06-02 16:14:06 +0300

    feat: add child functions

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,19 @@
-export const spawnChildProcess = async (args) => {
-    // Write your code here
+import { spawn } from "child_process";
+import { stdin, stdout } from "process";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+
+const settings = {
+  folder: "files",
+  file: "script.js",
 };
+
+export const spawnChildProcess = async (args) => {
+  const _dirname = dirname(fileURLToPath(import.meta.url));
+  const _filename = resolve(_dirname, settings.folder, settings.file);
+  const child = spawn("node", [_filename].concat(args));
+  stdin.pipe(child.stdin);
+  child.stdout.pipe(stdout);
+};
+
+spawnChildProcess(["--use-strict", "hello"]).then();
