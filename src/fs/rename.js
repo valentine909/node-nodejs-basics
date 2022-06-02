9a5f869abdb447451bb7eea027938fa202266a3b
Valentine <valentine.nazarov@gmail.com>
2022-06-02 09:45:59 +0300

    feat: add rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,22 @@
-export const rename = async () => {
-    // Write your code here 
+import fs from "fs/promises";
+import path from "path";
+import { fsError, exists } from "./helper.js";
+
+const settings = {
+  folder: "files",
+  file: "wrongFilename.txt",
+  newName: "properFilename.md",
 };
+
+export const rename = async () => {
+  const __dirname = path.resolve();
+  const file = path.resolve(__dirname, settings.folder, settings.file);
+  const renamed = path.resolve(__dirname, settings.folder, settings.newName);
+  if ((await exists(file)) && !(await exists(renamed))) {
+    await fs.rename(file, renamed);
+  } else {
+    throw fsError;
+  }
+};
+
+rename().then();
