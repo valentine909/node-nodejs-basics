9d4682a101f8d497abbf3a6b7b5e1c10dcee6d9f
Valentine <valentine.nazarov@gmail.com>
2022-06-02 12:21:23 +0300

    feat: add readStream function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,19 @@
-export const read = async () => {
-    // Write your code here 
+import { createReadStream } from "fs";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+import { stdout } from "process";
+
+const settings = {
+  folder: "files",
+  file: "fileToRead.txt",
 };
+
+export const read = async () => {
+  const _dirname = dirname(fileURLToPath(import.meta.url));
+  const _filename = resolve(_dirname, settings.folder, settings.file);
+  const stream = createReadStream(_filename);
+  stream.pipe(stdout);
+  stream.on("readable", () => stream.read());
+};
+
+read().then();
