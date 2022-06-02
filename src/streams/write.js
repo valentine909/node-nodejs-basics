c4c20908e2af4465d533257bc48e616a3eac88ed
Valentine <valentine.nazarov@gmail.com>
2022-06-02 13:45:09 +0300

    feat: add writeStream function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,18 @@
-export const write = async () => {
-    // Write your code here 
+import { createWriteStream } from "fs";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+import { stdin } from "process";
+
+const settings = {
+  folder: "files",
+  file: "fileToWrite.txt",
 };
+
+export const write = async () => {
+  const _dirname = dirname(fileURLToPath(import.meta.url));
+  const _filename = resolve(_dirname, settings.folder, settings.file);
+  const writer = createWriteStream(_filename, { encoding: "utf-8" });
+  stdin.pipe(writer);
+};
+
+write().then();
