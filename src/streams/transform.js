3c9d1776cc905c4c413c1013e510c99d525f600a
Valentine <valentine.nazarov@gmail.com>
2022-06-02 13:51:53 +0300

    feat: add transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,12 @@
+import { stdin, stdout } from "process";
+
 export const transform = async () => {
-    // Write your code here 
+  stdin.on("readable", () => {
+    const chunk = stdin.read();
+    if (chunk) {
+      stdout.write(chunk.reverse());
+    }
+  });
 };
+
+transform().then();
