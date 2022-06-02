0c133c5b8bae440b74c964e7718155e5fb0ecd33
Valentine <valentine.nazarov@gmail.com>
2022-06-02 09:18:30 +0300

    feat: add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,20 @@
-export const list = async () => {
-    // Write your code here 
+import fs from "fs/promises";
+import { fsError, exists } from "./helper.js";
+import path from "path";
+
+const settings = {
+  folder: "files",
 };
+
+export const list = async () => {
+  const __dirname = path.resolve();
+  const folder = path.resolve(__dirname, settings.folder);
+  if (await exists(folder)) {
+    const files = await fs.readdir(folder, { withFileTypes: true });
+    files.map((file) => console.log(file.name));
+  } else {
+    throw fsError;
+  }
+};
+
+list().then();
