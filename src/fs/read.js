b21f19d61952be1af878ca15b024b01fca17022a
Valentine <valentine.nazarov@gmail.com>
2022-06-02 09:25:07 +0300

    feat: add read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,22 @@
-export const read = async () => {
-    // Write your code here 
+import fs from "fs/promises";
+import path from "path";
+import { fsError, exists } from "./helper.js";
+
+const settings = {
+  folder: "files",
+  file: "fileToRead.txt",
 };
+
+export const read = async () => {
+  const __dirname = path.resolve();
+  const folder = path.resolve(__dirname, settings.folder);
+  const file = path.resolve(folder, settings.file);
+  if (await exists(file)) {
+    const content = await fs.readFile(file, { encoding: "utf-8" });
+    console.log(content);
+  } else {
+    throw fsError;
+  }
+};
+
+read().then();
